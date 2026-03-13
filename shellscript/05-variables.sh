f6c2ba2b0edd32f30dbf6288cfc9d556e451fabf
Your Name <your-email@example.com>
2026-03-13 20:54:03 +0530

    actions

---
+++ shellscript/05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#USE AND PRACTICE SPECIAL VARIABLE PROVIDED BY SHELL
+echo "All args passed to script $@"
+echo "No. of variables passed to script $#"
+echo "Name of tis script $0"
+echo "Present working directory is $PWD"
+echo "User who is running this script is $USER"
+echo "Home directory of user is $HOME"
+echo "PID of script is $PID"
+sleep 10 &
+echo "PID of recently executed process is $!"
+echo "All args passed to script is $*"
