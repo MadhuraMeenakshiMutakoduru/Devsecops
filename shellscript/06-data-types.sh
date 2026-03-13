fd7cdc820ec648cdbe26b270659d696aff880ee3
Your Name <your-email@example.com>
2026-03-13 21:10:20 +0530

    actions

--- shellscript/06-data-types.sh
+++ shellscript/06-data-types.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-
+#Number or strings
 NUM1=$1
 NUM2=$2
 
 TOTAL=$(($NUM1+$NUM2))
 echo "$TOTAL"
+
+#ARRAYS
+THINGS=("PEN" "PENCIL" "LAPTOP" "GUN")
+echo "Things are ${THINGS[@]}"
+echo "Things are ${THINGS[0]}"
+echo "Things are ${THINGS[1]}"
+echo "Things are ${THINGS[2]}"
+echo "Things are ${THINGS[3]}"
+
+
