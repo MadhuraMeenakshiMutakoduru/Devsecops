fcf221b2910c423936614a1a5da2461112d54004
Your Name <your-email@example.com>
2026-03-14 13:55:31 +0530

    actions

--- shellscript/07-conditions.sh
+++ shellscript/07-conditions.sh
@@ -4,4 +4,8 @@ NUMBER=90
 
 if [ $NUMBER -gt 18 ]; then
    echo "Given number :$NUMBER is greater than 18"
+elif [ $NUMBER -eq 20 ]; then
+   echo "Both are same numbers"
+else 
+   echo "Lessthan 18"
 fi 
